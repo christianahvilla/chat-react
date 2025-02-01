de926e3934551a92a1c856310854f4ae78e292cd
Christian Alejandro Herrejon Villa <christianahvilla@gmail.com>
2025-01-31 21:00:57 -0600

    Fix types

--- src/chat/chat.mock.ts
+++ src/chat/chat.mock.ts
@@ -5,30 +5,36 @@ export const MESSAGE_MOCKS: IGenericMessage[] = [
     id: 1,
     content: 'Hola, ¿cómo estás?',
     sender: 'Alice',
+    date: '',
   },
   {
     id: 2,
     content: 'MI mensaje',
     sender: 'Jose',
+    date: '',
   },
   {
     id: 3,
     content: '',
     sender: 'Jose',
+    date: '',
   },
   {
     id: 4,
     content: 'Mandando un saludo mi estimado',
     sender: 'Jose',
+    date: '',
   },
   {
     id: 5,
     content: 'Probando el mensaje',
     sender: 'Jose',
+    date: '',
   },
   {
     id: 6,
     content: 'Probando el mensaje',
     sender: 'Christian H',
+    date: '',
   },
 ];
